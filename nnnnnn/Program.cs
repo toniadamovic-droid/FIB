e54b2f3f4ab2b8239806b0dc7a3f2c3962f237d7
toniadamovic-droid <toni.adamovic@skole.hr>
2025-10-06 13:51:06 +0200

    Add files via upload

---
+++ nnnnnn/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 0;
+int z = 0;
+int zbroj = 0;
+Console.WriteLine("unesi broj n :");
+n = int.Parse(Console.ReadLine());
+
+do
+{
+    z++;
+    if (z % 2 == 0)
+    {
+        zbroj += zbroj;
+    }
+} while (zbroj < n);
+Console.WriteLine("ukupan Fibonaccijev zbroj je : {0}", zbroj);
